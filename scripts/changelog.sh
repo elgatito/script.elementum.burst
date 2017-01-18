193d39547d3a82d762a0e803d1e27bc6f3ebb660
scakemyer <scakemyer@tuta.io>
2017-01-18 08:48:12 -0500

    add dates to changelog

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -3,11 +3,11 @@ git tag -l | sort -u -r | while read TAG ; do
     if [ $NEXT ];then
         echo $NEXT
     fi
-    GIT_PAGER=cat git log --no-merges --format=" * %s" $TAG..$NEXT
+    GIT_PAGER=cat git log --no-merges --date-order --date=short --format=" * [%ad] %s" $TAG..$NEXT
     NEXT=$TAG
     echo
 done
 FIRST=$(git tag -l | head -1)
 echo $FIRST
-GIT_PAGER=cat git log --no-merges --format=" * %s" $FIRST
+GIT_PAGER=cat git log --no-merges --date-order --date=short --format=" * [%ad] %s" $FIRST
 echo
