5787ca26aff68809c888258a3a6fa0781c370c41
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2017-02-24 08:56:03 +0200

    added missing definitions after loading refactor

--- burst/providers/burst_overrides.py
+++ burst/providers/burst_overrides.py
@@ -130,19 +130,20 @@ overrides = {
 
     # Rutor
     'rutor': {
-        'anime_query': "0/10/100/2/QUERYEXTRA",
-        'movie_query': "0/1/100/2/QUERYEXTRA",
-        'season_query': "0/4/100/2/QUERYEXTRA",
-        'show_query': "0/4/100/2/QUERYEXTRA",
-        'season_keywords': "{title} [{season:2}x01",
-        'season_keywords2': "{title} [S{season:2}]",
+        'anime_query': "0/10/300/2/QUERYEXTRA",
+        'movie_query': "0/1/300/2/QUERYEXTRA",
+        'season_query': "0/4/300/2/QUERYEXTRA",
+        'show_query': "0/4/300/2/QUERYEXTRA",
+        'season_keywords': "{title} {season:2}x01|S{season:2}",
+        'season_keywords2': "",
         'tv_keywords': '{title} s{season:2}e{episode:2}',
-        'tv_keywords2': '{title} s{season:2}',
+        'tv_keywords2': '{title} {season:2}x01|S{season:2}',
         'parser': {
             'row': "find_once('table', order=3).find_all('tr', start=2)",
             'seeds': "item(tag='span', order=1, select=('class', 'green'))",
             'peers': "item(tag='span', order=1, select=('class', 'red'))",
-            'size': "item.find_all('td', ('align', 'right'))[-1].text()"
+            'size': "item.find_all('td', ('align', 'right'))[-1].text()",
+            'torrent': "'http://rutor.info%s' % (item(tag='a', order=1, attribute='href').split('.info', 1)[-1])"
         }
     },
 
