e6a4aed518cfa34e83529dd361de5c25c67268be
Gajdos Gábor <gazsiazasz@freemail.hu>
2018-02-27 14:39:29 +0100

    Corrected Hungarian providers
    Replaced bitHUmen provider logo.
    Changed nCore provider to get the passkey in the dl link.

--- burst/burst.py
+++ burst/burst.py
@@ -451,6 +451,12 @@ def extract_from_page(provider, content):
         log.debug('[%s] Matched download link with an ID: %s' % (provider, repr(result)))
         return result
 
+    matches = re.findall('torrents\.php\?action=download&id=\d+&key=[a-fA-F0-9]+', content)  # nCore
+    if matches:
+        result = definition['root_url'] + '/' + matches[0]
+        log.debug('[%s] Matched download link with an ID: %s' % (provider, repr(result)))
+        return result
+
     return None
 
 
