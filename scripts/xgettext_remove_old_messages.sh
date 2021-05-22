5b1cb473c4d805b4098d32c067e779336117d193
Anton Soroko <anton.soroko@gmail.com>
2021-05-23 00:38:41 +0300

    update scripts/xgettext_remove_old_messages.sh

--- scripts/xgettext_remove_old_messages.sh
+++ scripts/xgettext_remove_old_messages.sh
@@ -4,23 +4,24 @@ set -o nounset
 set -o errexit
 
 which rg > /dev/null || (echo "No ripgrep found, exit" && exit 1)
+[[ ! -d ../elementum ]] && (echo "No elementum folder found, exit" && exit 1)
 
 grep -oP 'msgctxt "#\K\d+' resources/language/messages.pot > ids.pot
 
 while read -r line; do rg -q -g '!*.po' -g '!*.pot' $line . || echo $line; done < ids.pot > orphaned_ids.pot
 
-find resources/ -name "*.po*" |
-while read -r file
+while read -r line; do rg -q -g '!*.po' -g '!*.pot' $line ../elementum || echo $line; done < orphaned_ids.pot > not_found_ids.pot
+
+while read -r line
 do
-    while read -r line
-    do
-        sed -i "/$line/,+3d" "$file"
-    done < orphaned_ids.pot
+    sed -i "/$line/,+3d" resources/language/messages.pot
+done < not_found_ids.pot
+
+# from https://stackoverflow.com/questions/10435926/how-to-automatcially-remove-unused-gettext-strings/26469640
+for file in resources/language/*/*.po
+do
+    msgattrib --no-wrap --set-obsolete --ignore-file=resources/language/messages.pot -o "$file" "$file"
+    msgattrib --no-wrap --no-obsolete -o "$file" "$file"
 done
 
-# TODO: https://stackoverflow.com/questions/10435926/how-to-automatcially-remove-unused-gettext-strings/26469640
-# after cleaning pot file we can use msgattrib to clean po files
-# msgattrib --set-obsolete --ignore-file=messages.pot -o messages.po messages.po
-# msgattrib --no-obsolete -o messages.po messages.po
-
-rm -f ids.pot orphaned_ids.pot
+rm -f ids.pot orphaned_ids.pot not_found_ids.pot
