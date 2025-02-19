2581e40f856967a08f91ba824ea4fa707b8401ce
Anton Soroko <anton.soroko@gmail.com>
2025-02-19 19:07:48 +0300

    Fix ilcorsaronero (new design), cpasbien, megapeer, newstudio, torrent9. (#445)
    Also improve queries for various tracker.

--- burst/burst.py
+++ burst/burst.py
@@ -637,10 +637,10 @@ def extract_from_page(provider, content):
             log.debug('[%s] Matched magnet info_hash search: %s' % (provider, repr(result)))
             return result
 
-        matches = re.findall(r'/download.php\?id=([A-Za-z0-9]{40})\W', content)
+        matches = re.findall(r'/download.php\?id=([A-Fa-f0-9]{40})\W', content)
         if matches:
             result = "magnet:?xt=urn:btih:" + matches[0]
-            log.debug('[%s] Matched download link: %s' % (provider, repr(result)))
+            log.debug('[%s] Matched magnet info_hash search: %s' % (provider, repr(result)))
             return result
 
         matches = re.findall(r'/engine/download.php\?id=[A-Za-z0-9]+[^\s\'"]*', content) # animaunt
@@ -656,7 +656,7 @@ def extract_from_page(provider, content):
             log.debug('[%s] Matched download link: %s' % (provider, repr(result)))
             return result
 
-        matches = re.findall(r'/get_torrent/([A-Fa-f0-9]{40})', content)
+        matches = re.findall(r'/get_torrents?/([A-Fa-f0-9]{40})', content)
         if matches:
             result = "magnet:?xt=urn:btih:" + matches[0]
             log.debug('[%s] Matched magnet info_hash search: %s' % (provider, repr(result)))
