df33416b1bd3cc1075652ba3afd89ea0bfffb481
Anton Soroko <anton.soroko@gmail.com>
2025-05-11 16:50:13 +0300

    Add antizapret usage.

--- burst/client.py
+++ burst/client.py
@@ -11,6 +11,7 @@ import os
 import urllib3
 import dns.resolver
 import requests
+import antizapret
 
 from elementum.provider import log, get_setting
 from time import sleep
@@ -135,12 +136,13 @@ class Client:
         self.passkey = None
         self.info = info
         self.proxy_url = None
+        self.use_antizapret = False
+        self.antizapret_proxy = None
         self.request_charset = request_charset
         self.response_charset = response_charset
         self.is_api = is_api
 
         self.use_cookie_sync = False
-        self.needs_proxylock = False
 
         self.headers = dict()
         self.request_headers = None
@@ -207,7 +209,7 @@ class Client:
                     self.proxy_url = "{0}://{1}:{2}@{3}:{4}".format(proxy['type'], proxy['login'], proxy['password'], proxy['host'], proxy['port'])
                 else:
                     self.proxy_url = "{0}://{1}:{2}".format(proxy['type'], proxy['host'], proxy['port'])
-            if proxy['use_type'] == 2 and info and "proxy_url" in info:
+            elif proxy['use_type'] == 2 and info and "proxy_url" in info:
                 log.debug("Setting proxy with hosts resolve from Elementum: %s" % (info["proxy_url"]))
 
                 proxy_url_scheme_separator = '://'
@@ -215,6 +217,12 @@ class Client:
                 elementum_proxy_url_prefix = elementum_proxy_url_parts[0].lower()
                 if elementum_proxy_url_prefix in elementum_proxy_types_overrides:
                     self.proxy_url = proxy_url_scheme_separator.join([elementum_proxy_types_overrides[elementum_proxy_url_prefix]] + elementum_proxy_url_parts[1:])
+            elif proxy['use_type'] == 3:
+                log.debug("Setting proxy to Antizapret proxy")
+
+                self.use_antizapret = True
+                self.proxy_url = None
+                self.antizapret_proxy = antizapret.AntizapretProxy()
         if self.proxy_url:
             self.session.proxies = {
                 'http': self.proxy_url,
@@ -285,6 +293,16 @@ class Client:
 
         if get_data:
             url += '?' + urlencode(get_data)
+
+        if self.use_antizapret:
+            parsed = urlparse(url)
+            proxy = self.antizapret_proxy.detect(host=parsed.netloc)
+            if proxy:
+                log.debug("Detected Antizapret proxy for %s: %s" % (parsed.netloc, proxy))
+                self.session.proxies = {
+                    'http': proxy,
+                    'https': proxy,
+                }
 
         log.debug("Opening URL: %s" % repr(url))
         if self.session.proxies:
